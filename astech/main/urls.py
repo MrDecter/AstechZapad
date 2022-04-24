1e72f4c1a6a05fdd4d9e65b0ecedbaaf69e5853a
Admin <ghoillus@gmail.com>
2022-04-24 15:34:02 +0300

    Создание шаблона HTML страницы, подключение CSS и создание тестовых страниц

--- astech/main/urls.py
+++ astech/main/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
 urlpatterns = [
-    path('', views.index)
+    path('', views.index, name='home'),
+    path('about-us', views.about, name='about')
 ]
--- astech/main/views.py
+++ astech/main/views.py
@@ -4,3 +4,6 @@ from django.http import HttpResponse
 
 def index(request):
     return render(request, 'main/index.html')
+
+def about(request):
+    return render(request, 'main/about-us.html')
